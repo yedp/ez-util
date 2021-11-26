7da13855b0d3f6348856ccbbd90e831de6bacec3
ydpup@126.com <5022Yedp>
2021-11-26 14:31:19 +0800

    添加字符串过滤表情包

--- src/main/java/com/github/yedp/ez/util/StringUtils.java
+++ src/main/java/com/github/yedp/ez/util/StringUtils.java
@@ -2,6 +2,7 @@ package com.github.yedp.ez.util;
 
 
 import java.io.UnsupportedEncodingException;
+import java.util.regex.Pattern;
 
 /**
  * @author yedp
@@ -12,6 +13,9 @@ public class StringUtils {
     private static final String CHARSET = "UTF-8";
     public static final String SPACE = " ";
     public static final String EMPTY = "";
+    //表情包表正则达式
+    public static final Pattern EMOJI_PATTERN = Pattern.compile("[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?", Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
+
 
     /**
      * <p>Checks if a CharSequence is whitespace, empty ("") or null.</p>
@@ -199,4 +203,46 @@ public class StringUtils {
     public static String bytesToStr(byte[] bytes, String charset) throws UnsupportedEncodingException {
         return new String(bytes, charset);
     }
+
+
+    /**
+     * <p>Checks if a CharSequence is has emoji.</p>
+     * <pre>
+     * StringUtils.hasEmoji("🍑")      = true
+     * StringUtils.hasEmoji("🎂")        = true
+     * StringUtils.hasEmoji("🧚‍♀")       = true
+     * StringUtils.hasEmoji("bob")     = false
+     * StringUtils.hasEmoji("  bob  ") = false
+     * </pre>
+     *
+     * @param cs the CharSequence to check, may be null
+     * @return {@code true} if the CharSequence is null, empty or whitespace
+     */
+    public static boolean hasEmoji(final CharSequence cs) {
+        if (cs == null || cs.length() == 0) {
+            return false;
+        }
+        return EMOJI_PATTERN.matcher(cs).find();
+    }
+
+    /**
+     * <p>remove all emoji from  String .</p>
+     * <pre>
+     * StringUtils.removeEmojis("A🍑")      = A
+     * StringUtils.removeEmojis("A🎂B")        = AB
+     * StringUtils.removeEmojis("🧚A‍♀")       = A
+     * StringUtils.removeEmojis("bob")     = bob
+     * StringUtils.removeEmojis("  bob  ") =   bob
+     * </pre>
+     *
+     * @param str the String to be remove emojis
+     * @return String without emoji
+     */
+    public static String removeEmojis(final String str) {
+        if (isBlank(str)) {
+            return str;
+        }
+        return EMOJI_PATTERN.matcher(str).replaceAll("").trim();
+    }
+
 }
