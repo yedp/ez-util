65768491b56f09367eb5a369bd513f21e924fb89
ydpup@126.com <5022Yedp>
2021-12-01 17:56:17 +0800

    企业微信工具

--- src/main/java/com/github/yedp/ez/util/QyWxUtil.java
+++ src/main/java/com/github/yedp/ez/util/QyWxUtil.java
@@ -5,6 +5,7 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 public class QyWxUtil {
     private static final String UPLOAD_URL = "https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?type=file&key=";
@@ -36,7 +37,53 @@ public class QyWxUtil {
         return StringUtils.EMPTY;
     }
 
-    public static String sendFileToGroup(String groupId, String fileId) throws IOException {
+    /**
+     * 发送文件到群
+     *
+     * @param groupId 群key
+     * @param fileId  文件id
+     * @return 结果
+     * @throws IOException 异常
+     */
+    public static String sendFile(String groupId, String fileId) throws IOException {
         return HttpUtil.ReturnPostBody(SEND_URL.concat(groupId), "{\"msgtype\":\"file\",\"file\":{\"media_id\":\"".concat(fileId).concat("\"}}"));
+    }
+
+
+    /**
+     * 发送消息
+     *
+     * @param groupId 群key
+     * @param msg     消息
+     * @return 结果
+     * @throws IOException 异常
+     */
+    public static String sendMsg(String groupId, String msg) throws IOException {
+        return HttpUtil.ReturnPostBody(SEND_URL.concat(groupId), "{\"msgtype\":\"text\",\"text\":{\"content\":\"".concat(msg).concat("\"}}"));
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param groupId       群key
+     * @param msg           消息
+     * @param mentionedPhoneList @需关注人的手机号码
+     * @return 结果
+     * @throws IOException 异常
+     */
+    public static String sendMsg(String groupId, String msg, List<String> mentionedPhoneList) throws IOException {
+        return HttpUtil.ReturnPostBody(SEND_URL.concat(groupId), "{\"msgtype\":\"text\",\"text\":{\"content\":\"".concat(msg).concat("\", \"mentioned_mobile_list\":" + JsonUtil.toString(mentionedPhoneList) + "}}"));
+    }
+
+    /**
+     * 发送消息markdown格式
+     *
+     * @param groupId 群key
+     * @param msg     消息
+     * @return 结果
+     * @throws IOException 异常
+     */
+    public static String sendMsgMarkdown(String groupId, String msg) throws IOException {
+        return HttpUtil.ReturnPostBody(SEND_URL.concat(groupId), "{\"msgtype\":\"markdown\",\"markdown\":{\"content\":\"".concat(msg).concat("\"}}"));
     }
 }
--- src/test/java/com/github/yedp/ez/util/codec/QyWxUtilTest.java
+++ src/test/java/com/github/yedp/ez/util/codec/QyWxUtilTest.java
@@ -5,17 +5,40 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QyWxUtilTest {
-    @Test
-    public void testUpload() throws IOException {
-//       String res = QyWxUtil.uploadFile("0cfe5b53-9676-4538-b685-8861bdfe9ab4", new File("d:/test.xlsx"));
+//    private String groupId = "0cfe5b53-9676-4538-b685-8861bdfe9***";
+//    @Test
+//    public void testUpload() throws IOException {
+//       String res = QyWxUtil.uploadFile(groupId, new File("d:/test.xlsx"));
 //       System.out.println(res);
-    }
-
-    @Test
-    public void tesSend() throws IOException {
-//        String res = QyWxUtil.sendFileToGroup("0cfe5b53-9676-4538-b685-8861bdfe9ab4", "3yYCulFcqkfUt6HuD0_VL3rzh9Y5cvshlMLFGGrSL0V7pCqFz8Z3Y4ogpulsMQSfy");
+//    }
+//
+//    @Test
+//    public void tesSendFile() throws IOException {
+//        String res = QyWxUtil.sendFile(groupId, "3yYCulFcqkfUt6HuD0_VL3rzh9Y5cvshlMLFGGrSL0V7pCqFz8Z3Y4ogpulsMQSfy");
 //        System.out.println(res);
-    }
+//    }
+//
+//    @Test
+//    public void tesSendMsg() throws IOException {
+//        String res = QyWxUtil.sendMsg(groupId, "测试消息");
+//        System.out.println(res);
+//    }
+//
+//    @Test
+//    public void tesSendMsgWithMentioned() throws IOException {
+//        List<String> mentionedList = new ArrayList<>();
+//        mentionedList.add("@all");
+//        String res = QyWxUtil.sendMsg(groupId, "测试消息",mentionedList);
+//        System.out.println(res);
+//    }
+//
+//    @Test
+//    public void tesSendMsgMarkdown() throws IOException {
+//        String res = QyWxUtil.sendMsgMarkdown(groupId, "测试**加粗**");
+//        System.out.println(res);
+//    }
 }
